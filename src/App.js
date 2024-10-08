5869d7a8c5b99acb5caa982c3906fb5541190bad
Gowda <shashi.gowda@capgemini.com>
2024-10-09 00:35:11 +0530

    todo-list commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Tasks from './Components/Tasks';
 import TodoApp from './Components/TodoApp';
 
 function App() {
   return (
     <div className="App">
-      <Tasks/>
       <TodoApp/>
     </div>
   );
